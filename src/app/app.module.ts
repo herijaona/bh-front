4f50fb903cc7addd6f7dbc4a308e9bfb35707e6f
RAKOTOVAO Safidison jean <safidison.rj@gmail.com>
2018-03-27 18:17:29 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomepageModule } from './homepage/homepage.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HomepageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
