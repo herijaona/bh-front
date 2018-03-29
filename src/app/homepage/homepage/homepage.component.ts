f72fd6c39b9321bb4867514be1fcece4b6d459e6
RAKOTOVAO Safidison jean <safidison.rj@gmail.com>
2018-03-29 15:16:45 +0300

    design homepage

--- src/app/homepage/homepage/homepage.component.ts
+++ src/app/homepage/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser'
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+  backgroundImg :string;
+  constructor(private sanitizer:DomSanitizer) {
+    
+   }
 
   ngOnInit() {
+    this.backgroundImg = 'http://dl27.fotosklad.org.ua/20121020/6d0d7b1596285466e8bb06114a88c903.jpg';
   }
 
 }
