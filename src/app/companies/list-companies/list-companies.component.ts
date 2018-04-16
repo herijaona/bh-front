9c7ec69a068cecadc95ffeaeb394c0f5820b9f5a
RAKOTOVAO Safidison jean <safidison.rj@gmail.com>
2018-04-16 18:09:42 +0300

    list companies

--- src/app/companies/list-companies/list-companies.component.ts
+++ src/app/companies/list-companies/list-companies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Globals } from '../../globals/globals';
 
 @Component({
   selector: 'app-list-companies',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListCompaniesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public g:Globals) { }
 
   ngOnInit() {
   }
