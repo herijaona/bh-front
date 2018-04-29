087855c1b90e777f00378e6f26883b9efe6e388a
Nhilo94 <fanilorakoto.94@gmail.com>
2018-04-29 10:59:52 +0300

    delete unused file and import

--- src/app/homepage/homepage/homepage.component.ts
+++ src/app/homepage/homepage/homepage.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from "@angular/core";
-import { BrowserModule, DomSanitizer } from "@angular/platform-browser";
 import { HomesliderComponent } from "../homeslider/homeslider.component";
 @Component({
-  selector: "app-homepage",
-  templateUrl: "./homepage.component.html",
-  styleUrls: ["./homepage.component.scss"]
+	selector: "app-homepage",
+	templateUrl: "./homepage.component.html",
+	styleUrls: ["./homepage.component.scss"]
 })
 export class HomepageComponent implements OnInit {
- 
-  constructor(private sanitizer: DomSanitizer) {}
+	constructor() {}
 
-  ngOnInit() {}
+	ngOnInit() {}
 }
