2ba0ab1ebc9b8160967c8860d7f39c976c9541d8
Nhilo94 <fanilorakoto.94@gmail.com>
2018-04-29 11:27:31 +0300

    add shared service for loading data on view

--- src/app/services/shared-notification/shared-notification.service.ts
+++ src/app/services/shared-notification/shared-notification.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs/Subject";
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 
 @Injectable()
 export class SharedNotificationService {
+	/*
+	* Load view : called when data change for all view reload their data
+	*/
+	private loadView = new BehaviorSubject<Object>({});
+
 	private loadingMessage = new BehaviorSubject<Object>({});
 	private headerUpdateMessage = new BehaviorSubject<Object>({});
 	private companyNotif = new BehaviorSubject<Object>({});
@@ -11,6 +16,12 @@ export class SharedNotificationService {
 	private DataReadyNotif = new BehaviorSubject<Object>({});
 	private updateViewNotif = new BehaviorSubject<Object>({});
 	// private missionConfirmedSource = new Subject<any>();
+
+	/*
+	* Load view : called when data change for all view reload their data
+	*/
+	viewLoad$ = this.toastNotif.asObservable();
+
 	run_loader$ = this.loadingMessage.asObservable();
 	run_runUpdateHeader$ = this.headerUpdateMessage.asObservable();
 	notifCompany$ = this.companyNotif.asObservable();
@@ -28,19 +39,26 @@ export class SharedNotificationService {
 		this.headerUpdateMessage.next(message);
 	}
 
-	notifDataUnvaliable(d : any){
+	notifDataUnvaliable(d: any) {
 		this.companyNotif.next(d);
 	}
 
-	notifyUpdateView(ew){
+	notifyUpdateView(ew) {
 		this.updateViewNotif.next(ew);
 	}
 
-	notifToast(arg : any ){
+	notifToast(arg: any) {
 		this.toastNotif.next(arg);
 	}
 
-	tellDataReady(arg : any ){
+	tellDataReady(arg: any) {
 		this.DataReadyNotif.next(arg);
+	}
+
+	/*
+	* Load view : called when data change for all view reload their data
+	*/
+	loadViewData(arg: any) {
+		this.loadView.next(arg);
 	}
 }
