409f345088184b7225139ab4346fd7e0148f4003
Nhilo94 <fanilorakoto.94@gmail.com>
2018-05-03 17:28:54 +0300

    presentation edit - on mindset page

--- src/app/mindset/mindset.module.ts
+++ src/app/mindset/mindset.module.ts
@@ -1,25 +1,36 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { MDBBootstrapModule } from "angular-bootstrap-md";
-import { MindsetComponent } from './mindset/mindset.component';
-import { PageHeaderComponent } from './page-header/page-header.component';
-import { EditPageButtonComponent } from './edit-page-button/edit-page-button.component';
-import { PresentationSideComponent } from './presentation-side/presentation-side.component';
-import { SectionEditButtonComponent } from './section-edit-button/section-edit-button.component';
-import { ImSelectComponent } from './im-select/im-select.component';
-import { QuillModule } from 'ngx-quill'
+import { MindsetComponent } from "./mindset/mindset.component";
+import { PageHeaderComponent } from "./page-header/page-header.component";
+import { EditPageButtonComponent } from "./edit-page-button/edit-page-button.component";
+import { PresentationSideComponent } from "./presentation-side/presentation-side.component";
+import { SectionEditButtonComponent } from "./section-edit-button/section-edit-button.component";
+import { ImSelectComponent } from "./im-select/im-select.component";
+import { QuillModule } from "ngx-quill";
+import { BhSafeHtmlPipe } from "../pipe/bh-safe-html.pipe";
+import { ZoneMindsetComponent } from './zone-mindset/zone-mindset.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    QuillModule,
-    FormsModule,
-    MDBBootstrapModule.forRoot()
-  ],
-  declarations: [MindsetComponent, PageHeaderComponent, EditPageButtonComponent, PresentationSideComponent, SectionEditButtonComponent, ImSelectComponent],
-  schemas: [NO_ERRORS_SCHEMA]
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		QuillModule,
+		FormsModule,
+		MDBBootstrapModule.forRoot()
+	],
+	declarations: [
+		MindsetComponent,
+		BhSafeHtmlPipe,
+		PageHeaderComponent,
+		EditPageButtonComponent,
+		PresentationSideComponent,
+		SectionEditButtonComponent,
+		ImSelectComponent,
+		ZoneMindsetComponent
+	],
+	schemas: [NO_ERRORS_SCHEMA]
 })
-export class MindsetModule { }
+export class MindsetModule {}
---
+++ src/app/mindset/zone-mindset/zone-mindset.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zone-mindset',
+  templateUrl: './zone-mindset.component.html',
+  styleUrls: ['./zone-mindset.component.scss']
+})
+export class ZoneMindsetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
