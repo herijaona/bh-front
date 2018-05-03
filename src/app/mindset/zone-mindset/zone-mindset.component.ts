7c6c3bb6384cbf21609d76d7964a3bcb8b711e10
Nhilo94 <fanilorakoto.94@gmail.com>
2018-05-03 18:22:47 +0300

    mindset zone

--- src/app/mindset/zone-mindset/zone-mindset.component.ts
+++ src/app/mindset/zone-mindset/zone-mindset.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Globals } from "./../../globals/globals";
+import { AuthserviceService } from "../../services/authservice/authservice.service";
+import { CompanyService } from "../../services/company/company.service";
+
 
 @Component({
-  selector: 'app-zone-mindset',
+  selector: 'zone-mindset',
   templateUrl: './zone-mindset.component.html',
   styleUrls: ['./zone-mindset.component.scss']
 })
 export class ZoneMindsetComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public g: Globals,
+    private auth: AuthserviceService,
+    private cs: CompanyService
+  ) {}
 
   ngOnInit() {
   }
