75c1749dfb38afa914d71fe2a7e5cde403215af8
safidison <safidison.rj@gmail.com>
2018-05-10 10:57:30 +0300

    projectAdd and list

--- src/app/globals/globals.ts
+++ src/app/globals/globals.ts
@@ -7,4 +7,62 @@ export class Globals {
 	api_baseUrl: string = "http://localhost:3000";
 	site_baseUrl: string = "http://localhost:4546";
 	urlArrayLeng: 6
+	public conf_editor = {
+		removePlugins : 'elementspath' ,
+		toolbar: [
+			{
+				name: "clipboard",
+				groups: ["clipboard", "undo"],
+				items: [
+					"Cut",
+					"Copy",
+					"Paste",
+					"PasteText",
+					"PasteFromWord",
+					"-",
+					"Undo",
+					"Redo"
+				]
+			},
+			{
+				name: "basicstyles",
+				groups: ["basicstyles", "cleanup"],
+				items: [
+					"Bold",
+					"Italic",
+					"Underline",
+					"Strike",
+					"Subscript",
+					"Superscript",
+					"-",
+					"CopyFormatting",
+					"RemoveFormat"
+				]
+			},
+			{
+				name: "paragraph",
+				groups: ["list", "indent", "blocks", "align", "bidi"],
+				items: [
+					"NumberedList",
+					"BulletedList",
+					"-",
+					"Outdent",
+					"Indent",
+					"-",
+					"Blockquote"
+				]
+			},
+			{ name: "links", items: ["Link", "Unlink"] },
+			{
+				name: "insert",
+				items: [
+					"HorizontalRule",
+					"Smiley",
+					"SpecialChar"
+				]
+			},
+			{ name: "styles", items: ["Styles", "Format", "FontSize"] },
+			{ name: "colors", items: ["TextColor", "BGColor"] },
+		]
+	};
 }
---
+++ src/app/projects/project-page-content/project-page-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-page-content',
+  templateUrl: './project-page-content.component.html',
+  styleUrls: ['./project-page-content.component.scss']
+})
+export class ProjectPageContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
