8459013e94294937c3ae1df031bc52f9ea03b67b
safidison <safidison.rj@gmail.com>
2018-05-12 10:03:04 +0300

    import module to profile-all

--- src/app/profile-all/profile-all.module.ts
+++ src/app/profile-all/profile-all.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { GeneralUtilitiesModule } from "../general-utilities/general-utilities.module";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+	imports: [
+		CommonModule,
+		GeneralUtilitiesModule,
+		ReactiveFormsModule,
+		RouterModule
+	],
+	declarations: []
 })
-export class ProfileAllModule { }
+export class ProfileAllModule {}
