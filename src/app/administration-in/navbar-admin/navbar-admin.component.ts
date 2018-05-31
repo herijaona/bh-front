4ba64bc177431eb8e75bece97d8fbb3013e4f0ca
safidison <safidison.rj@gmail.com>
2018-05-31 23:26:28 +0300

    web portal link

--- src/app/administration-in/navbar-admin/navbar-admin.component.ts
+++ src/app/administration-in/navbar-admin/navbar-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, ElementRef } from "@angular/core";
 import { Globals } from "./../../globals/globals";
 import { Router } from "@angular/router";
+import { CompanyService } from "../../services/company/company.service";
 
 @Component({
 	selector: "navbar-admin",
@@ -10,15 +11,33 @@ import { Router } from "@angular/router";
 export class NavbarAdminComponent implements OnInit {
 	public show: boolean = false;
 	public hide: boolean = false;
+	public currentSlug : string = '';
 
-	constructor(private router: Router, public g: Globals, public el: ElementRef) {}
-
-	ngOnInit() {
-		
+	constructor(
+		private cs: CompanyService,
+		private router: Router,
+		public g: Globals,
+		public el: ElementRef
+	) {
+		this.getDataOnCompany();
 	}
+
+	ngOnInit() {}
 	toggleCollapse() {
-		this.el.nativeElement.querySelector(".mobil-top").classList.toggle("toggle-in");
-		this.el.nativeElement.querySelector(".mobil-top").classList.replace("nav-link", "fy");
+		this.el.nativeElement
+			.querySelector(".mobil-top")
+			.classList.toggle("toggle-in");
+		this.el.nativeElement
+			.querySelector(".mobil-top")
+			.classList.replace("nav-link", "fy");
 	}
 
+	async getDataOnCompany() {
+		let getRes = await this.cs.getMyCompanData();
+		if (getRes) {
+			if (getRes.hasOwnProperty("_slug")) {
+				this.currentSlug = getRes["_slug"];
+			}
+		}
+	}
 }
