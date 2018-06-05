3f1f8e00445cffe0d1f9f0398346f44c3b7f3b2e
safidison <safidison.rj@gmail.com>
2018-06-05 20:22:55 +0300

    change some typing

--- src/app/company-data/company-data.module.ts
+++ src/app/company-data/company-data.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { ApplicationReportComponent } from "./application-report/application-report.component";
-import { GeneralUtilitiesModule } from "../general-utilities/general-utilities.module";
-import { RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApplicationReportComponent } from './application-report/application-report.component';
+import { GeneralUtilitiesModule } from '../general-utilities/general-utilities.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-	imports: [CommonModule, GeneralUtilitiesModule, RouterModule],
-	declarations: [ApplicationReportComponent]
+  imports: [CommonModule, GeneralUtilitiesModule, RouterModule],
+  declarations: [ApplicationReportComponent]
 })
 export class CompanyDataModule {}
