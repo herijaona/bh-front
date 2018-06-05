6a0d6b3b68045e937546145df4a2bd04b74a2ba4
safidison <safidison.rj@gmail.com>
2018-06-05 09:10:34 +0300

    delete community from team

--- src/app/administration-in/members-admin/members-admin.component.ts
+++ src/app/administration-in/members-admin/members-admin.component.ts
@@ -5,162 +5,157 @@ import { Globals } from "./../../globals/globals";
 import { AuthserviceService } from "../../services/authservice/authservice.service";
 import { TeamsService } from "../../services/teams/teams.service";
 @Component({
-	selector: "members-admin",
-	templateUrl: "./members-admin.component.html",
-	styleUrls: ["./members-admin.component.scss"]
+  selector: "members-admin",
+  templateUrl: "./members-admin.component.html",
+  styleUrls: ["./members-admin.component.scss"]
 })
 export class MembersAdminComponent implements OnInit {
-	public team_page: string = "team_page";
-	public details: any;
-	public img_avatar: string;
-	public activated: boolean = false;
-	public readyData: boolean = false;
-	public userAdminData: any;
-	public userCommData: any;
-	public inviteForm: FormGroup;
+  public team_page: string = "team_page";
+  public details: any;
+  public img_avatar: string;
+  public activated: boolean = false;
+  public readyData: boolean = false;
+  public userAdminData: any;
+  public userCommData: any;
+  public inviteForm: FormGroup;
 
-	public adminAll: boolean = true;
-	public communityShow: boolean = false;
-	constructor(
-		public g: Globals,
-		private router: Router,
-		private tms: TeamsService,
-		private auth: AuthserviceService
-	) {
-		this.img_avatar = this.g.base_href + "assets/img/profile.JPG";
-		this.inviteForm = new FormGroup({
-			lastname: new FormControl("", [Validators.required]),
-			firstname: new FormControl("", [Validators.required]),
-			invAsTeam: new FormControl(true),
-			invAsComm: new FormControl(true),
-			email: new FormControl("", [
-				Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$"),
-				Validators.required
-			])
-		});
-	}
-	async ngOnInit() {
-		try {
-			let isAdmin = await this.auth.isAdminUser();
-			if (isAdmin.status == "OK") {
-				this.getProfile();
-				this.getMember();
-				this.getCommunity();
-			} else {
-				this.router.navigateByUrl("/");
-			}
-		} catch (e) {}
-	}
-	async getProfile() {
-		try {
-			let prdata: any = await this.auth.profile();
-			if (prdata) {
-				if (prdata.active) {
-					this.activated = true;
-					this.details = prdata;
-					this.readyData = true;
-					return prdata;
-				} else {
-					throw new Object({
-						type: "NotActivate"
-					});
-				}
-			}
-		} catch (ee) {
-			this.readyData = true;
-			if ("type" in ee) {
-				if (ee.type == "NotActivate") {
-					this.activated = false;
-				}
-			} else {
-				this.auth.logout();
-			}
-		}
-	}
-	gotoProfileEdit() {
-		this.router.navigateByUrl("/administration-in/user/profile");
-	}
-	async submitinvite() {
-		var data: any = this.inviteForm.value;
-		var teamInviteRes: any = await this.tms.inviteTeam(data);
-	}
+  public adminAll: boolean = true;
+  public communityShow: boolean = false;
+  constructor(
+    public g: Globals,
+    private router: Router,
+    private tms: TeamsService,
+    private auth: AuthserviceService
+  ) {
+    this.img_avatar = this.g.base_href + "assets/img/profile.JPG";
+    this.inviteForm = new FormGroup({
+      lastname: new FormControl("", [Validators.required]),
+      firstname: new FormControl("", [Validators.required]),
+      invAsTeam: new FormControl(true),
+      invAsComm: new FormControl(true),
+      email: new FormControl("", [
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$"),
+        Validators.required
+      ])
+    });
+  }
+  async ngOnInit() {
+    try {
+      let isAdmin = await this.auth.isAdminUser();
+      if (isAdmin.status == "OK") {
+        this.getProfile();
+        this.getMember();
+      } else {
+        this.router.navigateByUrl("/");
+      }
+    } catch (e) {}
+  }
+  async getProfile() {
+    try {
+      let prdata: any = await this.auth.profile();
+      if (prdata) {
+        if (prdata.active) {
+          this.activated = true;
+          this.details = prdata;
+          this.readyData = true;
+          return prdata;
+        } else {
+          throw new Object({
+            type: "NotActivate"
+          });
+        }
+      }
+    } catch (ee) {
+      this.readyData = true;
+      if ("type" in ee) {
+        if (ee.type == "NotActivate") {
+          this.activated = false;
+        }
+      } else {
+        this.auth.logout();
+      }
+    }
+  }
+  gotoProfileEdit() {
+    this.router.navigateByUrl("/administration-in/user/profile");
+  }
+  async submitinvite() {
+    try {
+      var data: any = this.inviteForm.value;
+      var teamInviteRes: any = await this.tms.inviteTeam(data);
+      if (teamInviteRes.status == "OK") {
+        console.log("ok");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	async getMember() {
-		try {
-			let team_data: any = await this.tms.getTeamData();
-			if (team_data) {
-				this.userAdminData = team_data.data;
-			}
-		} catch (e) {
-			console.log(e);
-		}
-	}
+  async getMember() {
+    try {
+      let team_data: any = await this.tms.getTeamData();
+      if (team_data) {
+        this.userAdminData = team_data.data;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-	async getCommunity() {
-		try {
-			let cUser: any = await this.tms.getCommunityData();
-			if (cUser) {
-				if (cUser.status == "OK") {
-					this.userCommData = cUser.data.users;
-				}
-			}
-		} catch (e) {}
-	}
+  async teamCommChange(ev, ent, it) {
+    console.log(ent);
+    try {
+      let d: {
+        [key: string]: any;
+      } = {};
+      if (ent == "tAdm") {
+        d = {
+          _id: it._id,
+          value: !it.isAdm,
+          reg: "adm"
+        };
+      } else if (ent == "comm") {
+        d = {
+          _id: it._id,
+          value: !it.isComm,
+          reg: "com"
+        };
+      }
+      let r: any = await this.tms.changeRoleAdmin(d);
+      if (r) {
+        if (r.status == "OK") {
+          console.log("Success");
+          this.userAdminData = [];
+          this.getMember();
+        }
+      }
+    } catch (e) {}
+  }
+  async deleteFromTeamList(it) {
+    try {
+      let rm: any = await this.tms.deleteFromTeamList({ usr_id: it._id });
+      if (rm) {
+        if (rm.status == "OK") {
+          this.userAdminData = [];
+          this.getMember();
+        }
+      }
+    } catch (e) {}
+  }
 
-	async teamCommChange(ev, ent, it) {
-		console.log(ent);
-		try {
-			let d: {
-				[key: string]: any;
-			} = {};
-			if (ent == "tAdm") {
-				d = {
-					_id: it._id,
-					value: !it.isAdm,
-					reg: "adm"
-				};
-			} else if (ent == "comm") {
-				d = {
-					_id: it._id,
-					value: !it.isComm,
-					reg: "com"
-				};
-			}
-			let r: any = await this.tms.changeRoleAdmin(d);
-			if (r) {
-				if (r.status == "OK") {
-					console.log("Success");
-					this.userAdminData = [];
-					this.getMember();
-				}
-			}
-		} catch (e) {}
-	}
-	async deleteFromTeamList(it) {
-		try {
-			let rm: any = await this.tms.deleteFromTeamList({ usr_id: it._id });
-			if (rm) {
-				if (rm.status == "OK") {
-					this.userAdminData = [];
-					this.getMember();
-				}
-			}
-		} catch (e) {}
-	}
-
-	viewUsersTeams(scp) {
-		switch (scp) {
-			case "admin":
-				this.adminAll = true;
-				this.communityShow = false;
-				break;
-			case "community":
-				this.adminAll = false;
-				this.communityShow = true;
-				break;
-			default:
-				// code...
-				break;
-		}
-	}
+  viewUsersTeams(scp) {
+    switch (scp) {
+      case "admin":
+        this.adminAll = true;
+        this.communityShow = false;
+        break;
+      case "community":
+        this.adminAll = false;
+        this.communityShow = true;
+        break;
+      default:
+        // code...
+        break;
+    }
+  }
 }
