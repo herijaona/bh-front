c6cc1c28c30b5ef377aef37324ab77474818a424
safidison <safidison.rj@gmail.com>
2018-06-07 18:18:25 +0300

    application sent

--- src/app/administration-in/collaborations/application-sent/application-sent.component.ts
+++ src/app/administration-in/collaborations/application-sent/application-sent.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../../../services/projects/projects.service';
 
 @Component({
-  selector: "app-application-sent",
-  templateUrl: "./application-sent.component.html",
-  styleUrls: ["./application-sent.component.scss"]
+  selector: 'app-application-sent',
+  templateUrl: './application-sent.component.html',
+  styleUrls: ['./application-sent.component.scss'],
 })
 export class ApplicationSentComponent implements OnInit {
-  constructor() {}
+  public allApplData: any = [];
+  constructor(private pr: ProjectsService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getAllCApplicationSent();
+  }
+  async getAllCApplicationSent() {
+    this.allApplData = [];
+    try {
+      const appl: any = await this.pr.getUserApplicationSent();
+      if (appl.status === 'OK') {
+        this.allApplData = appl.data;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
