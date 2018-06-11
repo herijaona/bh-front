a0d2d9c452acc39a8e044dfbae760207a845dfac
safidison <safidison.rj@gmail.com>
2018-06-11 17:08:04 +0300

    page  login modif

--- src/app/user-auth/page-login/page-login.component.ts
+++ src/app/user-auth/page-login/page-login.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { AuthserviceService } from "../../services/authservice/authservice.service";
-import { Router } from "@angular/router";
-import { SharedNotificationService } from "./../../services/shared-notification/shared-notification.service";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthserviceService } from '../../services/authservice/authservice.service';
+import { Router } from '@angular/router';
+import { SharedNotificationService } from './../../services/shared-notification/shared-notification.service';
 
-import { Globals } from "./../../globals/globals";
+import { Globals } from './../../globals/globals';
 @Component({
-  selector: "page-login",
-  templateUrl: "./page-login.component.html",
-  styleUrls: ["./page-login.component.scss"]
+  selector: 'page-login',
+  templateUrl: './page-login.component.html',
+  styleUrls: ['./page-login.component.scss'],
 })
 export class PageLoginComponent implements OnInit {
   public img_logo: string;
@@ -17,8 +17,8 @@ export class PageLoginComponent implements OnInit {
   public resetpassForm: FormGroup;
   public notifReset: boolean = false;
   public loginFormFlag: boolean = true;
-  type_ = "notif";
-  text_ = "Success de registration";
+  type_ = 'notif';
+  text_ = 'Success de registration';
   error_log: boolean = false;
   public img_bg: string;
   constructor(
@@ -27,44 +27,35 @@ export class PageLoginComponent implements OnInit {
     private router: Router,
     private sh: SharedNotificationService
   ) {
-    this.img_avatar = this.g.base_href + "assets/img/bg-accueil.jpg";
-    this.img_logo = this.g.base_href + "assets/img/bh.png";
-    this.img_bg = this.g.base_href + "assets/img/bg-0.png";
+    this.img_avatar = this.g.base_href + 'assets/img/bg-accueil.jpg';
+    this.img_logo = this.g.base_href + 'assets/img/bh.png';
+    this.img_bg = this.g.base_href + 'assets/img/bg-0.png';
   }
   ngOnInit() {
     this.resetpassForm = new FormGroup({
-      bhemail: new FormControl("", [
-        Validators.required,
-        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$")
-      ])
+      bhemail: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]),
     });
 
     this.loginForm = new FormGroup({
-      bhemail: new FormControl("", [
-        Validators.required,
-        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$")
-      ]),
-      bh_pass: new FormControl("", [
-        Validators.required,
-        Validators.minLength(8)
-      ])
+      bhemail: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]),
+      bh_pass: new FormControl('', [Validators.required, Validators.minLength(8)]),
     });
   }
   onFormSubmit() {
     let credential = {
       email: this.loginForm.value.bhemail,
-      password: this.loginForm.value.bh_pass
+      password: this.loginForm.value.bh_pass,
     };
     this.auth.login(credential).then(
       (data: any) => {
         this.auth.profile().then(
           (res: any) => {
-            this.router.navigateByUrl("/administration-in/desk");
+            this.router.navigateByUrl('/administration-in/desk');
           },
           err => {
             this.sh.notifToast({
-              type: "warning",
-              message: "<p>Erreur inattendu</p>"
+              type: 'warning',
+              message: '<p>Erreur inattendu</p>',
             });
 
             setTimeout(() => {
@@ -76,7 +67,7 @@ export class PageLoginComponent implements OnInit {
       error => {
         this.error_log = true;
         this.text_ = error.message;
-        this.type_ = "danger";
+        this.type_ = 'danger';
       }
     );
   }
@@ -101,29 +92,23 @@ export class PageLoginComponent implements OnInit {
         .then(
           (res: any) => {
             setTimeout(() => {
-              if (res.status == "OK") {
-                this.type_ = "success";
+              if (res.status == 'OK') {
+                this.type_ = 'success';
                 this.text_ =
-                  "Demande de reinitialisation de mot passe effectuer avec success <br>" +
+                  'Demande de reinitialisation de mot passe effectuer avec success <br>' +
                   res.message +
-                  " <br> Veuiller consulter votre email .";
+                  ' <br> Veuiller consulter votre email .';
               } else {
-                this.type_ = "warning";
-                this.text_ =
-                  "Un erreur est survenue au cours de votre demande<br>" +
-                  res.message +
-                  " <br> Merci.";
+                this.type_ = 'warning';
+                this.text_ = 'Un erreur est survenue au cours de votre demande<br>' + res.message + ' <br> Merci.';
               }
               this.resetpassForm.reset();
               this.error_log = true;
             }, 1000);
           },
           error => {
-            this.type_ = "warning";
-            this.text_ =
-              "Un erreur est survenue au cours de votre demande<br>" +
-              error.error.text +
-              " <br> Merci.";
+            this.type_ = 'warning';
+            this.text_ = 'Un erreur est survenue au cours de votre demande<br>' + error.error.text + ' <br> Merci.';
             this.resetpassForm.reset();
             this.error_log = true;
           }
@@ -131,8 +116,7 @@ export class PageLoginComponent implements OnInit {
     });
   }
 
-  getUrl()
-	{
-	  return "url(" + this.img_bg + ")";
-	}
+  getUrl() {
+    return 'url(' + this.img_bg + ')';
+  }
 }
