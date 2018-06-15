4ea2881530e1ce6679e97d3bbfdfc79f44af75ed
safidison <safidison.rj@gmail.com>
2018-06-15 12:30:12 +0300

    prepare for others type of collaboration

--- src/app/administration-in/collaborations/application-received/application-received.component.ts
+++ src/app/administration-in/collaborations/application-received/application-received.component.ts
@@ -1,33 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Globals } from './../../../globals/globals';
-import { AuthserviceService } from '../../../services/authservice/authservice.service';
-import { TeamsService } from '../../../services/teams/teams.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Globals } from "./../../../globals/globals";
+import { ProjectsService } from "../../../services/projects/projects.service";
+import { Title } from '@angular/platform-browser';
 
-import { ProjectsService } from '../../../services/projects/projects.service';
 
 @Component({
-  selector: 'app-application-received',
-  templateUrl: './application-received.component.html',
-  styleUrls: ['./application-received.component.scss'],
+  selector: "app-application-received",
+  templateUrl: "./application-received.component.html",
+  styleUrls: ["./application-received.component.scss"]
 })
 export class ApplicationReceivedComponent implements OnInit {
-  public applicationreport_page = 'applicationreport_page';
   public allApplData: any = [];
-  public dataCollab: any = {};
-  public byApplication: boolean = false;
-  private currentCollabID = '';
-  constructor(private pr: ProjectsService, private activRoute: ActivatedRoute) {}
+  constructor(
+    private pr: ProjectsService,
+    public g : Globals,
+    private titl  : Title
+  ) {
+    this.titl.setTitle('All Received Application');
+  }
   ngOnInit() {
-    this.activRoute.params.subscribe((params_: any) => {
-      this.currentCollabID = params_['idCollab'];
-      console.log(this.currentCollabID);
-      if (this.currentCollabID) {
-        this.getAllApplicationByCollab(this.currentCollabID);
-      } else {
-        this.getAllCProjectApplication();
-      }
-    });
+    this.getAllCProjectApplication();
   }
   /**
    * getAllCProjectApplication
@@ -41,19 +33,6 @@ export class ApplicationReceivedComponent implements OnInit {
       }
     } catch (e) {
       console.log(e);
-    }
-  }
-
-  async getAllApplicationByCollab(cCollabID) {
-    try {
-      const applyByCollab = await this.pr.getApplByCollabID(cCollabID);
-      if (applyByCollab['status'] === 'OK') {
-        this.byApplication = true;
-        this.allApplData = applyByCollab['data']['allApplication'];
-        this.dataCollab = applyByCollab['data']['collabData'];
-      }
-    } catch (error) {
-      console.log(error);
     }
   }
 }
