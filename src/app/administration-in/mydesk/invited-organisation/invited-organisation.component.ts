3f9b86d78e62dbd9cf6700e1c8864a143f4536ca
safidison <safidison.rj@gmail.com>
2018-06-19 07:09:14 +0300

    invite send data api

--- src/app/administration-in/mydesk/invited-organisation/invited-organisation.component.ts
+++ src/app/administration-in/mydesk/invited-organisation/invited-organisation.component.ts
@@ -1,40 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormArray,
+  FormBuilder,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { Globals } from "../../../globals/globals";
+import { SharedNotificationService } from "../../../services/shared-notification/shared-notification.service";
+import { AuthserviceService } from "../../../services/authservice/authservice.service";
+import { TeamsService } from "../../../services/teams/teams.service";
+
 @Component({
-  selector: 'app-invited-organisation',
-  templateUrl: './invited-organisation.component.html',
-  styleUrls: ['./invited-organisation.component.scss']
+  selector: "app-invited-organisation",
+  templateUrl: "./invited-organisation.component.html",
+  styleUrls: ["./invited-organisation.component.scss"]
 })
 export class InvitedOrganisationComponent implements OnInit {
-  public invoiceForm: FormGroup;
+  public inviteForm: FormGroup;
   public itemrow: any;
 
-  constructor(private _fb: FormBuilder) { }
+  constructor(private _fb: FormBuilder, private tms: TeamsService) {}
 
   ngOnInit() {
-    this.invoiceForm = this._fb.group({
-      itemRows: this._fb.array([this.initItemRows()])
+    this.inviteForm = this._fb.group({
+      inviteItem: this._fb.array([this.initItemRows()])
     });
   }
 
   initItemRows() {
     return this._fb.group({
-      itemname: ['']
+      firstname: new FormControl("", [Validators.required]),
+      lastname: new FormControl("", [Validators.required]),
+      organisationName: new FormControl("", [Validators.required]),
+      bhEmail: new FormControl("", [
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$"),
+        Validators.required
+      ])
     });
   }
 
   addNewRow() {
-    const control = <FormArray>this.invoiceForm.controls['itemRows'];
+    const control = <FormArray>this.inviteForm.controls["inviteItem"];
     control.push(this.initItemRows());
-
   }
   deleteRow(index: number) {
-    const control = <FormArray>this.invoiceForm.controls['itemRows'];
+    const control = <FormArray>this.inviteForm.controls["inviteItem"];
     control.removeAt(index);
   }
   getcontrols() {
-    return this.invoiceForm.controls.itemRows['controls'];
+    return this.inviteForm.controls.inviteItem["controls"];
   }
 
+  async sendInvitation() {
+    if (!this.inviteForm.valid) {
+      return;
+    }
+    let argDATA = {
+      org_data: this.inviteForm.value.inviteItem
+    };
+    try {
+      let invitRes = await this.tms.sendOrgnisationInvitation(argDATA);
+      if (invitRes["status"] == "OK") {
+        console.log("OK");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
--- src/app/services/teams/teams.service.ts
+++ src/app/services/teams/teams.service.ts
@@ -1,97 +1,110 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BaseHttpService } from '../base-http/base-http.service';
-import { SharedNotificationService } from '../shared-notification/shared-notification.service';
-import { Globals } from './../../globals/globals';
-import { INTERNAL_BROWSER_PLATFORM_PROVIDERS } from '@angular/platform-browser/src/browser';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { BaseHttpService } from "../base-http/base-http.service";
+import { SharedNotificationService } from "../shared-notification/shared-notification.service";
+import { Globals } from "./../../globals/globals";
+import { INTERNAL_BROWSER_PLATFORM_PROVIDERS } from "@angular/platform-browser/src/browser";
 
 @Injectable()
 export class TeamsService extends BaseHttpService {
-  constructor(public http: HttpClient, public g: Globals, public sh: SharedNotificationService) {
+  constructor(
+    public http: HttpClient,
+    public g: Globals,
+    public sh: SharedNotificationService
+  ) {
     super(http, g, sh);
   }
 
   teamFrontSaveData(arg: any) {
-    return this.fetch('post', 'team_front_video', arg).toPromise();
+    return this.fetch("post", "team_front_video", arg).toPromise();
   }
 
   teamFrontGetData(arg: any) {
-    return this.fetch('get', 'team_front_video', {
-      company_slug: arg,
+    return this.fetch("get", "team_front_video", {
+      company_slug: arg
     }).toPromise();
   }
 
   deleteTmV(arg) {
-    return this.fetch('delete', 'team_front_video', {
-      tm_video_id: arg,
+    return this.fetch("delete", "team_front_video", {
+      tm_video_id: arg
     }).toPromise();
   }
 
   updatetmvData(arg) {
-    return this.fetch('put', 'team_front_video', arg).toPromise();
+    return this.fetch("put", "team_front_video", arg).toPromise();
   }
 
   public inviteTeam(da) {
-    return this.fetch('post', 'invite-in-team', da).toPromise();
+    return this.fetch("post", "invite-in-team", da).toPromise();
   }
 
   public getTeamUsers(slug) {
-    return this.fetch('get', 'teams-users', {
-      company_slug: slug,
+    return this.fetch("get", "teams-users", {
+      company_slug: slug
     }).toPromise();
   }
 
   public getUsersTeamsNameFn(arg1, arg2) {
-    return this.fetch('get', 'team-details', { id_user: arg1, accountID: arg2 }).toPromise();
+    return this.fetch("get", "team-details", {
+      id_user: arg1,
+      accountID: arg2
+    }).toPromise();
   }
 
   public questionsSendData(arg) {
-    return this.fetch('post', 'question-data', arg).toPromise();
+    return this.fetch("post", "question-data", arg).toPromise();
   }
 
   public getTeamData() {
-    return this.fetch('get', 'teamsUsers').toPromise();
+    return this.fetch("get", "teamsUsers").toPromise();
   }
 
   public changeRoleAdmin(arg) {
-    return this.fetch('put', 'change_roleAdm', arg).toPromise();
+    return this.fetch("put", "change_roleAdm", arg).toPromise();
   }
 
   public deleteFromTeamList(arg) {
-    return this.fetch('delete', 'delete-from-team', arg).toPromise();
+    return this.fetch("delete", "delete-from-team", arg).toPromise();
   }
 
   public getCommunityData() {
-    return this.fetch('get', 'getAccountCommunity').toPromise();
+    return this.fetch("get", "getAccountCommunity").toPromise();
   }
 
   public getAllQuestionsOnCompany(type) {
-    return this.fetch('get', 'getallCompanyQuestions', {
-      qtype: type,
+    return this.fetch("get", "getallCompanyQuestions", {
+      qtype: type
     }).toPromise();
   }
   public getAllArchivesOnCompany(type) {
-    return this.fetch('get', 'getallCompanyArchives', {
-      qtype: type,
+    return this.fetch("get", "getallCompanyArchives", {
+      qtype: type
     }).toPromise();
   }
 
   public archiveQuestions(idQ) {
-    return this.fetch('post', 'archives_questions', { idQ: idQ }).toPromise();
+    return this.fetch("post", "archives_questions", { idQ: idQ }).toPromise();
   }
   public sendResponseTOQuestions(arg) {
-    return this.fetch('post', 'reply_questions', arg).toPromise();
+    return this.fetch("post", "reply_questions", arg).toPromise();
   }
   public getDetailsOnQuestion(qID) {
-    return this.fetch('get', 'getDetailOnQuestion', {
-      qID: qID,
+    return this.fetch("get", "getDetailOnQuestion", {
+      qID: qID
     }).toPromise();
   }
 
   public getAllInvitationSent() {
-    return this.fetch('get', 'getInvitationSent').toPromise();
+    return this.fetch("get", "getInvitationSent").toPromise();
   }
   public reviveInvitation(invID) {
-    return this.fetch('post', 'revive-invitation', { invID: invID }).toPromise();
+    return this.fetch("post", "revive-invitation", {
+      invID: invID
+    }).toPromise();
+  }
+
+  public sendOrgnisationInvitation(argDATA) {
+    return this.fetch("post", "invite_organisation", argDATA).toPromise();
   }
 }
