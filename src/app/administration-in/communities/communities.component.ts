29beb008eb40407d82cb2af871ef901aeb72e4b0
safidison <safidison.rj@gmail.com>
2018-06-22 07:24:20 +0300

    refac sonar

--- src/app/administration-in/communities/communities.component.ts
+++ src/app/administration-in/communities/communities.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { Globals } from "./../../globals/globals";
-import { Router } from "@angular/router";
-import { AuthserviceService } from "../../services/authservice/authservice.service";
-import { TeamsService } from "../../services/teams/teams.service";
+import { Component, OnInit } from '@angular/core';
+import { Globals } from './../../globals/globals';
+import { Router } from '@angular/router';
+import { AuthserviceService } from '../../services/authservice/authservice.service';
+import { TeamsService } from '../../services/teams/teams.service';
 
 @Component({
-	selector: "app-communities",
-	templateUrl: "./communities.component.html",
-	styleUrls: ["./communities.component.scss"]
+  selector: 'app-communities',
+  templateUrl: './communities.component.html',
+  styleUrls: ['./communities.component.scss'],
 })
 export class CommunitiesComponent implements OnInit {
-	constructor() {}
-	ngOnInit() {}
+  constructor() {}
+  ngOnInit() {}
 }
