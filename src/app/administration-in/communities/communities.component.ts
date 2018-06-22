414d7093c1d8aea2bac50067df6274c47f5f3f95
nilho94 <fanilorakoto.94@gmail.com>
2018-06-22 13:53:10 +0300

    tableau sent and one collaboration

--- src/app/administration-in/communities/communities.component.ts
+++ src/app/administration-in/communities/communities.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { Globals } from "./../../globals/globals";
 import { Router } from "@angular/router";
 import { AuthserviceService } from "../../services/authservice/authservice.service";
 import { TeamsService } from "../../services/teams/teams.service";
-
+import { ModalDirective } from 'angular-bootstrap-md';
 @Component({
 	selector: "app-communities",
 	templateUrl: "./communities.component.html",
 	styleUrls: ["./communities.component.scss"]
 })
 export class CommunitiesComponent implements OnInit {
-	constructor() {}
+	public img_a: string;
+	@ViewChild('modalHist') public myModalHist: ModalDirective;
+	constructor(
+		public g: Globals) {
+		this.img_a = this.g.base_href + 'assets/img/group.svg';
+	}
 	ngOnInit() {}
+
+	public showMod(item) {
+		setTimeout(() => {
+		  this.myModalHist.show();
+		}, 330);
+	  }
+	  public hideModal() {
+		setTimeout(() => {
+		  this.myModalHist.hide();
+		}, 330);
+	  }
 }
