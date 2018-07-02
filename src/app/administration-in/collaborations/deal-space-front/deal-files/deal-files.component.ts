efae4345fa86a5d09b9b2a8729acdeb2f902be04
nilho94 <fanilorakoto.94@gmail.com>
2018-07-02 17:54:49 +0300

    deal space integration

--- src/app/administration-in/collaborations/deal-space-front/deal-files/deal-files.component.ts
+++ src/app/administration-in/collaborations/deal-space-front/deal-files/deal-files.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalDirective } from 'angular-bootstrap-md';
 @Component({
   selector: 'app-deal-files',
   templateUrl: './deal-files.component.html',
   styleUrls: ['./deal-files.component.scss']
 })
 export class DealFilesComponent implements OnInit {
-
+  @ViewChild('modalHist') public myModalHist: ModalDirective;
   constructor() { }
 
   ngOnInit() {
+  }
+  public insertObservation() {
+    setTimeout(() => {
+      this.myModalHist.show();
+    }, 330);
+  }
+  public hideModal() {
+    setTimeout(() => {
+      this.myModalHist.hide();
+    }, 330);
   }
 
 }
